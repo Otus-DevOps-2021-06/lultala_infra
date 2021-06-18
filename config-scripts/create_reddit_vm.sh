839ba98ffda55569faae36bfab675c81f1af58e1
lultala <svarog-corp@ya.ru>
2021-06-18 20:42:26 +0300

    edit scripts create vm

--- config-scripts/create_reddit_vm.sh
+++ config-scripts/create_reddit_vm.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 yc compute instance create \
   --name reddit-full-vm \
   --zone ru-central1-a \
-  --create-boot-disk name=disk1,size=15,image-id=fd892l6aqsdcg3m833t2 \
+  --create-boot-disk name=disk1,size=15,image-id=fd8ta1u3fg29kjc7vjfm \
   --public-ip \
   --ssh-key ~/.ssh/appuser.pub
