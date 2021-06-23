52e9b22fa1cdc01d6a45dd9287e4eee91d946db6
lultala <svarog-corp@ya.ru>
2021-06-23 12:47:37 +0300

    ansible - 2

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -4,7 +4,7 @@ resource "yandex_vpc_network" "app-network" {
 
 resource "yandex_vpc_subnet" "app-subnet" {
   name           = "reddit-app-subnet"
-  zone           = "ru-central1-a"
+  zone           = "ru-central1-b"
   network_id     = "${yandex_vpc_network.app-network.id}"
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -26,3 +26,4 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+
